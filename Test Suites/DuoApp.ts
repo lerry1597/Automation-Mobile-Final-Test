b05ae04d28771b88cd0fba77d7817e9d34315fc9
7105021510970001 <lerrylalogiroth10@gmail.com>
2023-05-14 13:05:44 +0700

    first commit with project final test

---
+++ Test Suites/DuoApp.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DuoApp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d629c55b-7ba2-4e31-b82e-5b80be66effe</testSuiteGuid>
+   <testCaseLink>
+      <guid>58c90d6b-ab33-4682-8167-1555140d9fea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Calculator/ChoosTheme</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
